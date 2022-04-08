49d7d3a470abaa8cc45f013e1b90f8bec52ec535
Volker Christian <volker.christian@fh-hagenberg.at>
2022-04-08 11:10:36 +0200

    Extend server to be a "chatapp"

--- server.cpp
+++ server.cpp
@@ -1,10 +1,72 @@
-#include <snode.c/core/SNodeC.h>
-#include <snode.c/core/socket/SocketContextFactory.h>
-#include <snode.c/net/in/stream/legacy/SocketServer.h>
+#include <core/SNodeC.h>
+#include <core/socket/SocketContext.h>
+#include <core/socket/SocketContextFactory.h>
+#include <list>
+#include <net/in/stream/legacy/SocketServer.h>
+
+class ChatApp {
+private:
+    ChatApp() = default;
+    ChatApp(const ChatApp&) = delete;
+    ChatApp& operator=(const ChatApp&) = delete;
+
+public:
+    static ChatApp& instance() {
+        static ChatApp chat;
+
+        return chat;
+    }
+
+    void addServerContext(core::socket::SocketContext* serverContext) {
+        serverContexts.push_back(serverContext);
+    }
+
+    void removeServerContext(core::socket::SocketContext* serverContext) {
+        serverContexts.remove(serverContext);
+    }
+
+    void echoToAll(const std::string& text) {
+        for (core::socket::SocketContext* serverContext : serverContexts) {
+            serverContext->sendToPeer(text);
+        }
+    }
+
+private:
+    std::list<core::socket::SocketContext*> serverContexts;
+};
+
+class ServerContext : public core::socket::SocketContext {
+public:
+    explicit ServerContext(core::socket::SocketConnection* socketConnection)
+        : core::socket::SocketContext(socketConnection) {
+    }
+
+private:
+    void onConnected() override {
+        std::cout << "Context connected to Peer" << std::endl;
+        ChatApp::instance().addServerContext(this);
+    }
+
+    void onDisconnected() override {
+        std::cout << "Context disconnecte4d from Peer" << std::endl;
+        ChatApp::instance().removeServerContext(this);
+    }
+
+    void onReceiveFromPeer() override {
+        char buffer[1024];
+
+        ssize_t numBytesRead = readFromPeer(buffer, 1023);
+        buffer[numBytesRead] = 0;
+
+        std::cout << "Buffer: " << buffer << std::endl;
+
+        ChatApp::instance().echoToAll(std::string(buffer, numBytesRead));
+    }
+};
 
 class ServerContextFactory : public core::socket::SocketContextFactory {
-    core::socket::SocketContext* create([[maybe_unused]] core::socket::SocketConnection* socketConnection) {
-        return nullptr;
+    core::socket::SocketContext* create(core::socket::SocketConnection* socketConnection) {
+        return new ServerContext(socketConnection);
     }
 };
 
@@ -24,6 +86,14 @@ int main(int argc, char* argv[]) {
         [](SocketConnection* socketConnection) -> void {
             std::cout << "OnDisconnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
         });
+
+    server.listen(8080, 5, [](const Server::SocketAddress& socketAddress, int errnum) -> void {
+        if (errnum == 0) {
+            std::cout << "Server listening on " << socketAddress.toString() << std::endl;
+        } else {
+            std::cout << "Error: Server trying to listen on " << socketAddress.toString() << " : errno = " << errnum;
+        }
+    });
 
     return core::SNodeC::start();
 }
