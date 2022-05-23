f899da7f203e450ba14038cbf0d98a8320671b06
Volker Christian <volker.christian@fh-hagenberg.at>
2022-05-23 12:20:13 +0200

    Secure Server-Client communication

--- client.cpp
+++ client.cpp
@@ -1,27 +1,58 @@
 #include "ClientContext.h"
 
 #include <core/SNodeC.h>
+#include <net/in/stream/tls/SocketClient.h>
 #include <net/un/stream/legacy/SocketClient.h>
 #include <string>
 
 int main(int argc, char* argv[]) {
-    using Client = net::un::stream::legacy::SocketClient<ClientContextFactory>;
-    using SocketConnection = Client::SocketConnection;
+    //    using Client = net::un::stream::legacy::SocketClient<ClientContextFactory>;
+    //    using SocketConnection = Client::SocketConnection;
 
     core::SNodeC::init(argc, argv);
+    /*
+        Client client(
+            [](SocketConnection* socketConnection) -> void {
+                std::cout << "OnConnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+            },
+            [](SocketConnection* socketConnection) -> void {
+                std::cout << "OnConnected from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+            },
+            [](SocketConnection* socketConnection) -> void {
+                std::cout << "OnDisconnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+            });
 
-    Client client(
-        [](SocketConnection* socketConnection) -> void {
+        client.connect("/tmp/testsocket", [](const Client::SocketAddress& socketAddress, int errnum) -> void {
+            if (errnum == 0) {
+                std::cout << "Client connected to " << socketAddress.toString() << std::endl;
+            } else {
+                std::cout << "Error: Client trying to connect to " << socketAddress.toString() << " : errno = " << errnum;
+            }
+        });
+    */
+
+    using ClientTLS = net::in::stream::tls::SocketClient<ClientContextFactory>;
+    using SocketConnectionTLS = ClientTLS::SocketConnection;
+
+    std::map<std::string, std::any> options;
+    options["CertChain"] = "/home/voc/projects/ServerClient1/Client-End-Entity.pem";
+    options["CertChainKey"] = "/home/voc/projects/ServerClient1/Client-End-Entity-Key.pem";
+    options["Password"] = "pentium5";
+    options["CaFile"] = "/home/voc/projects/ServerClient1/VolkerChristianRootCA.pem";
+
+    ClientTLS clienttls(
+        [](SocketConnectionTLS* socketConnection) -> void {
             std::cout << "OnConnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
         },
-        [](SocketConnection* socketConnection) -> void {
+        [](SocketConnectionTLS* socketConnection) -> void {
             std::cout << "OnConnected from: " << socketConnection->getRemoteAddress().toString() << std::endl;
         },
-        [](SocketConnection* socketConnection) -> void {
+        [](SocketConnectionTLS* socketConnection) -> void {
             std::cout << "OnDisconnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
-        });
+        },
+        options);
 
-    client.connect("/tmp/testsocket", [](const Client::SocketAddress& socketAddress, int errnum) -> void {
+    clienttls.connect("localhost", 8082, [](const ClientTLS::SocketAddress& socketAddress, int errnum) -> void {
         if (errnum == 0) {
             std::cout << "Client connected to " << socketAddress.toString() << std::endl;
         } else {
--- server.cpp
+++ server.cpp
@@ -1,14 +1,19 @@
 #include "ChatApp.h"
 #include "ServerContext.h"
 
+#include <any>
 #include <core/SNodeC.h>
+#include <map>
 #include <net/in/stream/legacy/SocketServer.h>
+#include <net/in/stream/tls/SocketServer.h>
 #include <net/in6/stream/legacy/SocketServer.h>
 #include <net/un/stream/legacy/SocketServer.h>
+#include <set>
 #include <string>
 
 int main(int argc, char* argv[]) {
     core::SNodeC::init(argc, argv);
+
     using Server = net::in::stream::legacy::SocketServer<ServerContextFactory>;
     using SocketConnection = net::in::stream::legacy::SocketServer<ServerContextFactory>::SocketConnection;
 
@@ -28,6 +33,35 @@ int main(int argc, char* argv[]) {
             std::cout << "Server listening on " << socketAddress.toString() << std::endl;
         } else {
             std::cout << "Error: Server trying to listen on " << socketAddress.toString() << " : errno = " << errnum << std::endl;
+        }
+    });
+
+    using ServerTLS = net::in::stream::tls::SocketServer<ServerContextFactory>;
+    using SocketConnectionTLS = net::in::stream::tls::SocketServer<ServerContextFactory>::SocketConnection;
+
+    std::map<std::string, std::any> options;
+    options["CertChain"] = "/home/voc/projects/ServerClient1/WebServerCertificateChain.pem";
+    options["CertChainKey"] = "/home/voc/projects/ServerClient1/Volker_Christian_-_WEB-Cert.pem";
+    options["Password"] = "pentium5";
+    options["CaFile"] = "/home/voc/projects/ServerClient1/Client-Root-CA.crt";
+
+    ServerTLS servertls(
+        [](SocketConnectionTLS* socketConnection) -> void {
+            std::cout << "OnConnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+        },
+        [](SocketConnectionTLS* socketConnection) -> void {
+            std::cout << "OnConnected from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+        },
+        [](SocketConnectionTLS* socketConnection) -> void {
+            std::cout << "OnDisconnect from: " << socketConnection->getRemoteAddress().toString() << std::endl;
+        },
+        options);
+
+    servertls.listen(8082, 5, [](const ServerTLS::SocketAddress& socketAddress, int errnum) -> void {
+        if (errnum == 0) {
+            std::cout << "TLS-Server listening on " << socketAddress.toString() << std::endl;
+        } else {
+            std::cout << "Error: TLS-Server trying to listen on " << socketAddress.toString() << " : errno = " << errnum << std::endl;
         }
     });
 
